7f3564daba42c413a6e6727dbb34f34712c31c19
Jabran Rafique <hello@jabran.me>
2015-10-27 09:40:09 +0000

    update structure

--- test/exception/FooBarExceptionTest.php
+++ test/exception/FooBarExceptionTest.php
@@ -1,12 +1,14 @@
 <?php namespace JRI\Tests;
 
+use \JRI\Exception\FooBarException;
+
 class FooBarExceptionTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @expectedException \JRI\Exception\FooBarException
 	 */
 	public function testFooBarException() {
-		throw new \JRI\Exception\FooBarException('foo');
+		throw new FooBarException('foo');
 	}
 }
 
